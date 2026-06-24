280bba0a5005a193e4b91217da45c023c16a04e5
adeel-chaudharey <adeelabbaschaudharey@gmail.com>
2026-06-24 21:44:12 +0500

    connected next.js to the supabase

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
   /* config options here */
 };
 
